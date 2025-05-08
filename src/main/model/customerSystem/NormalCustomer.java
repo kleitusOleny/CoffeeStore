1937e224a1cfc54c74d0cb32e6ea9433588d56b4
Truc350 <23130350@st.hcmuaf.edu.vn>
2025-05-08 19:19:48 +0700

    them model 2 system : reservationSystem va CustomerSystem thực hiện tất cả phương thức cho phần khách hàng.Thêm lớp Date cho lớp reservationSystem

--- src/main/model/customerSystem/NormalCustomer.java
+++ src/main/model/customerSystem/NormalCustomer.java
@@ -1,17 +1,47 @@
 package model.customerSystem;
 
-public class NormalCustomer extends Customer{
-    public NormalCustomer(String name, String idCus, String numsPhone) {
-        super(name, idCus, numsPhone);
+public class NormalCustomer extends Customer {
+    private boolean flag;
+
+    public NormalCustomer(String name, String idCus, String numsPhone, boolean flag, boolean flag1) {
+        super(name, idCus, numsPhone, flag);
+        this.flag = false;
+    }
+
+    public boolean isFlag() {
+        return flag;
     }
 
     @Override
     public void updateNotify(String nameTB, String ndTB) {
+        if (isRegister() == true){
+            System.out.println("Thông báo mới cho khách hàng: "+this.name+": ");
+            System.out.println("Tiêu đề: "+nameTB);
+            System.out.println("Nội dung: "+ndTB);
+        }else {
+            System.out.println("Bạn cần đăng ký để có thể nhận được thông báo từ cửa hàng! ");
+        }
 
     }
 
     @Override
     public void updatePoint(int point1) {
 
+    }
+
+    @Override
+    public String getType() {
+        return "Normal";
+    }
+
+
+    @Override
+    public boolean isRegister() {
+        return flag == true;
+    }
+
+    @Override
+    public int countProductedBuy() {
+        return 0;
     }
 }
