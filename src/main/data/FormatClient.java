c9a400206da86a6484a79507f8f8a63d9f726520
JukisYuri <yurichangirl752@gmail.com>
2025-05-21 13:41:16 +0700

    Added discount data and made significant fixes in DiscountPanel, but the overwrite method has not been implemented yet.

---
+++ src/main/data/FormatClient.java
@@ -0,0 +1,26 @@
+package data;
+
+public class FormatClient {
+    private String hoTen, soDienThoai, diemTichLuy, trangThai;
+    private boolean chon;
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public String getDiemTichLuy() {
+        return diemTichLuy;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public boolean isChon() {
+        return chon;
+    }
+}
---
+++ src/main/data/FormatDiscount.java
@@ -0,0 +1,30 @@
+package data;
+
+public class FormatDiscount {
+    private String maKM, tenKM, noiDung, ngayBatDau, ngayKetThuc;
+    private boolean chon;
+
+    public String getMaKM() {
+        return maKM;
+    }
+
+    public String getTenKM() {
+        return tenKM;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public String getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public String getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public boolean isChon() {
+        return chon;
+    }
+}
